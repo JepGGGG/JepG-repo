f62fee8f92a891ac76a7e3afd6bfd3fa0eece9a9
JepG <chuypg1520@gmail.com>
2024-04-29 00:57:55 -0600

    Subo mis prim eras pruebas de este proyecto

---
+++ Master.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char namne;
+    int numero1;
+    printf("cual es tu nombre?");
+    scanf("%c",&namne);
+    printf("mucho gusto %c",namne);
+    
+    printf("dime los numeros a ingresar");  scanf("%d",&numero1);
+    return 0;
+}

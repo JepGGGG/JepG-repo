51a1fa5daeb794252cd9901e69c65067b0fc9149
JepG <chuypg1520@gmail.com>
2024-04-29 20:37:25 -0600

    agregamos variables base menor y base mayor

--- Master.c
+++ Master.c
@@ -1,11 +1,32 @@
 #include<stdio.h>
 int main(){
-    char namne;
-    int numero1;
-    printf("cual es tu nombre?");
-    scanf("%c",&namne);
-    printf("mucho gusto %c",namne);
-    
-    printf("dime los numeros a ingresar");  scanf("%d",&numero1);
-    return 0;
+  int Base,BaseMayor,BaseMenor,Altura;
+  int op;
+  float area;
+  printf("ingrese que quiere calcular\n");
+  printf("1)Area de un triangulo \n");
+  printf("2)Area de un trapecio \n");
+  printf("3)Area de un rectangulo\n");
+  scanf("%d",&op);
+  
+  switch(op){
+  	case 1:
+  		printf("Seleccionado Area de un triangulo \n");
+  		printf("ingrese base \n"); scanf("%d",&Base);
+  		printf("ingrese Altura \n"); scanf("%d",&Altura);
+  		area=(Base*Altura)/2;
+  		printf("El area del triangulo es de %.2f \n",area);
+  		break;
+  	case 2:
+  		printf("Seleccionado Area de un trapecio \n");
+  		printf("ingrese base Mayor \n"); scanf("%d",&BaseMayor);
+  		printf("ingrese base Menor \n"); scanf("%d",&BaseMenor);
+  		printf("ingrese Altura \n"); scanf("%d",&Altura);
+  		area=(BaseMayor+BaseMenor)*(Altura)/2;
+  		printf("El area del trapecio es de %.2f \n",area);
+  		break;
+  	case 3:
+  		printf("Seleccionado Area de un rectangulo \n");
+  		break;
+  }
 }
